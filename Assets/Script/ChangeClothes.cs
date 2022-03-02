07cdb982236611bf3cdc671cf4c7b169b1f4cba8
bautd.wiicamp <bautd@wiicamp.com>
2022-03-02 16:45:45 +0700

    refs task [FE] Download File

--- Assets/Script/ChangeClothes.cs
+++ Assets/Script/ChangeClothes.cs
@@ -13,6 +13,8 @@ public class ChangeClothes : MonoBehaviour
 
     GameObject[] listGameObjectFromAPI;
 
+    GameObject thePlayer;
+
     // Link list object:
     // https://ipfs.infura.io/ipfs/QmfENn7ZWZyXsGSxBaNwoYyaV94mzbiD2MXLkwkBnNDNry
 
@@ -31,6 +33,8 @@ public class ChangeClothes : MonoBehaviour
     {
         string urlJSON = "https://ipfs.infura.io/ipfs/QmfENn7ZWZyXsGSxBaNwoYyaV94mzbiD2MXLkwkBnNDNry";
 
+        thePlayer = GameObject.FindGameObjectsWithTag("Player")[0];
+
         UnityWebRequest request = UnityWebRequest.Get(urlJSON);
         request.chunkedTransfer = false;
         yield return request.SendWebRequest();
@@ -44,22 +48,88 @@ public class ChangeClothes : MonoBehaviour
             if (request.isDone)
             {
                 listClothes = JsonHelper.GetArray<SingleCloth>(request.downloadHandler.text);
-                StartCoroutine(FetchGameObject());
+                FetchGameObject();
             }
         }
     }
 
-    IEnumerator FetchGameObject()
+    [Obsolete]
+    void FetchGameObject()
     {
         for (int i = 0; i < listClothes.Length; i++)
         {
-            Debug.Log(listClothes[i].name);
-            string url3DObject = listClothes[i].link;
-            UnityWebRequest request3DObject = UnityWebRequest.Get(url3DObject);
-            yield return request3DObject.SendWebRequest();
+            //Debug.Log(listClothes[i].name);
+            int currentIndex = i;
 
-            byte[] result = request3DObject.downloadHandler.data;
-            Debug.Log(result);
+            StartCoroutine(GetFileRequest(listClothes[currentIndex].link, (UnityWebRequest req) =>
+            {
+                if (req.isNetworkError || req.isHttpError)
+                {
+                    // Log any errors that may happen
+                    Debug.Log($"{req.error} : {req.downloadHandler.text}");
+                }
+                else
+                {
+                    LoadModel(req.downloadHandler, listClothes[currentIndex].category);
+                }
+            }));
+
+            //StartCoroutine(GetModelAsset(listClothes[currentIndex].link, (AssetBundle bundle) =>
+            //{
+            //    LoadAssetBundle(bundle, listClothes[currentIndex].category);
+            //}));
+
+        }
+    }
+
+    void LoadModel(DownloadHandler fileObject, string categoryOf)
+    {
+        foreach (Transform child in thePlayer.transform)
+        {
+            Debug.Log(child.gameObject.name);
+        }
+    }
+
+    void LoadAssetBundle(AssetBundle fileObject, string categoryOf)
+    {
+        foreach (Transform child in thePlayer.transform)
+        {
+            //Debug.Log(child.gameObject.name);
+            if (child.gameObject.name == categoryOf)
+            {
+
+            }
+        }
+
+    }
+
+    IEnumerator GetFileRequest(string url, Action<UnityWebRequest> callback)
+    {
+        using (UnityWebRequest req = UnityWebRequest.Get(url))
+        {
+            req.downloadHandler = new DownloadHandlerFile(url);
+            yield return req.SendWebRequest();
+            callback(req);
+        }
+    }
+
+
+    IEnumerator GetModelAsset(string urlAssetBundle, Action<AssetBundle> callback)
+    {
+        UnityWebRequest www = new UnityWebRequest(urlAssetBundle);
+        DownloadHandlerAssetBundle handler = new DownloadHandlerAssetBundle(www.url, uint.MaxValue);
+        www.downloadHandler = handler;
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Extracts AssetBundle
+            AssetBundle bundle = handler.assetBundle;
+            callback(bundle);
         }
     }
 
